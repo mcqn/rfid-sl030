84e38b7a4605c7eeba40d0c47941bce5ce8c21c7
neil winterburn <neilwinterburn@gmail.com>
2014-10-17 19:47:08 +0100

    Added ability to authenticate with a tag and read blocks from it

---
+++ example/read_tag.js
@@ -0,0 +1,36 @@
+// Simple test program to show use of the rfid-sl018 module
+// (c) Copyright 2014 MCQN Ltd
+//
+// Tries to read a tag.  If one is detected, it prints out some basic
+// details about the tag.
+//
+// See README.md for more details.
+// Usage:
+//    sudo node scan_tag.js
+
+var rfid_sl018 = require('rfid-sl018');
+
+// Create and initialise a reader
+var rfid = new rfid_sl018.RFID_SL018();
+rfid.init();
+
+// Try to find a tag
+var tag = rfid.selectTag();
+
+if (tag) {
+    console.log("Got tag:");
+    console.log("- UID: "+tag.tagID.inspect());
+    console.log("- UID string: "+tag.tagIDString);
+    console.log("- Type: "+tag.tagType);
+
+    var block = 3;
+    // Need to authenticate first
+    if (rfid.authenticate(rfid.sectorForBlock(block))) {
+        console.log(rfid.readBlock(block));
+    } else {
+        console.log("Failed to authenticate");
+    }
+} else {
+    console.log("Couldn't read tag");
+}
+
